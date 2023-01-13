cb0a0ac8f2d2d8d86fc0dacf3ec9a704953f4e6e
Aiven Timptner <aiven@timptner.de>
2023-01-13 23:27:15 +0100

    Style landing page

--- home/models.py
+++ home/models.py
@@ -6,8 +6,18 @@ from wagtail.admin.panels import FieldPanel
 
 
 class HomePage(Page):
-    body = RichTextField(blank=True)
+    body = RichTextField(
+        features=[
+            'h3', 'h4', 'h5', 'h6',
+            'bold', 'italic',
+            'ol', 'ul',
+            'hr',
+            'link',
+        ],
+        blank=True)
+    image = models.ForeignKey('wagtailimages.Image', on_delete=models.SET_NULL, null=True, blank=True, related_name='+')
 
     content_panels = Page.content_panels + [
         FieldPanel('body'),
+        FieldPanel('image'),
     ]
